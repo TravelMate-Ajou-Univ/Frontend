7fa18eaae499025799d46e5e7bd9c5e356f116fc
김준하 <junhakjh@gmail.com>
2023-09-04 23:43:57 +0900

    init project

---
+++ src/app/layout.tsx
@@ -0,0 +1,18 @@
+import './globals.css';
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+  title: 'Travel Mate',
+  description: '실시간 채팅으로 여행 정보를 공유하세요.',
+};
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang='en'>
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
