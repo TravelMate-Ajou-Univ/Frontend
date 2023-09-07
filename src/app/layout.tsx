50f569c315196216615676e54a2b7ec70de7acc1
Sungpyo <pwsusc10@naver.com>
2023-09-07 19:26:57 +0900

    modify tsconfig, nextconfig

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,22 @@
-import './globals.css';
-import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Travel Mate',
-  description: '실시간 채팅으로 여행 정보를 공유하세요.',
+  title: "Travel Mate",
+  description: "실시간 채팅으로 여행 정보를 공유하세요.",
 };
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
-    <html lang='en'>
-      <body className={inter.className}>{children}</body>
+    <html lang="en" className={inter.className}>
+      <body>{children}</body>
     </html>
   );
 }
