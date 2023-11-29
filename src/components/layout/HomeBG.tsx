0e11b5a30b97f1e893452d1525b44e4a28ecf21f
김준하 <junhakjh@gmail.com>
2023-11-29 11:35:35 +0900

    chore: FadeReveal 주석 제거

--- src/components/layout/HomeBG.tsx
+++ src/components/layout/HomeBG.tsx
@@ -14,23 +14,22 @@ export default function HomeBG() {
         height={1080}
       />
       <div className="absolute top-[25%] left-[15%]">
-        {/* TODO: 배포할 때 주석 없애기 */}
-        {/* <FadeReveal> */}
-        <p className="text-secondary text-7xl font-semibold">TravelMate</p>
-        <p className="text-sky text-3xl mt-3">
-          모두가 만들어가는 <b>여행 플랫폼</b>
-        </p>
-        <div className="absolute top-20 -right-52">
-          <Image src={tooltip} alt="툴팁" width={150} height={150} />
-          <p className="w-full absolute top-[3.3rem] text-xs text-center">
-            <span className="text-blue-700">수정요청</span>을 해보세요.
-            <br />
-            <span className="text-blue-700">다같이</span> 여행기록을
-            <br />
-            꾸며보세요!
+        <FadeReveal>
+          <p className="text-secondary text-7xl font-semibold">TravelMate</p>
+          <p className="text-sky text-3xl mt-3">
+            모두가 만들어가는 <b>여행 플랫폼</b>
           </p>
-        </div>
-        {/* </FadeReveal> */}
+          <div className="absolute top-20 -right-52">
+            <Image src={tooltip} alt="툴팁" width={150} height={150} />
+            <p className="w-full absolute top-[3.3rem] text-xs text-center">
+              <span className="text-blue-700">수정요청</span>을 해보세요.
+              <br />
+              <span className="text-blue-700">다같이</span> 여행기록을
+              <br />
+              꾸며보세요!
+            </p>
+          </div>
+        </FadeReveal>
       </div>
     </div>
   );
