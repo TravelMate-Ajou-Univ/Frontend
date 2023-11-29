000796d7f0382696c8d52ea3b32e5cef8b1b11ea
김준하 <junhakjh@gmail.com>
2023-11-29 23:28:13 +0900

    design: 게시글 목록 반응형 디자인 적용

--- src/components/SharePostingTitle.tsx
+++ src/components/SharePostingTitle.tsx
@@ -3,10 +3,14 @@ import travelIcon from "/public/image/travelIcon.png";
 
 export default function SharePostingTitle() {
   return (
-    <h1 className="flex flex-row gap-3 my-4">
-      <Image className="w-12" src={travelIcon} alt="쉐어 포스팅 아이콘" />
+    <h1 className="flex flex-row gap-3 md:my-4 my-2">
+      <Image
+        className="md:w-12 w-10"
+        src={travelIcon}
+        alt="쉐어 포스팅 아이콘"
+      />
       <section className="flex flex-col justify-end">
-        <p className="text-2xl font-bold">쉐어 포스팅</p>
+        <p className="md:text-2xl text-xl font-bold">쉐어 포스팅</p>
         <p className="text-sm text-gray-500">
           모두가 함께 만들어 나가는 여행 정보 게시판
         </p>
--- src/components/article/list/ArticleSummary.tsx
+++ src/components/article/list/ArticleSummary.tsx
@@ -12,18 +12,18 @@ export default function ArticleSummary({
   count = -1
 }: Props) {
   return (
-    <article className="flex flex-col gap-2 p-4">
-      <h1 className="text-lg font-semibold">
+    <article className="flex flex-col gap-2 md:p-4 px-2 py-1">
+      <h1 className="md:text-lg text-base font-semibold">
         {title}
         {count !== -1 && (
-          <span className="border rounded-full px-1 text-red-500 text-sm font-normal ml-2">
+          <span className="border rounded-full px-1 text-red-500 md:text-sm text-xs font-normal ml-2">
             +{count}
           </span>
         )}
       </h1>
       <ul>
         {keywords.map(keyword => (
-          <li className="inline-block mr-2.5 mb-1" key={keyword.id}>
+          <li className="inline-block md:mr-2.5 mr-1 mb-1" key={keyword.id}>
             <Keyword keyword={keyword.name} />
           </li>
         ))}
