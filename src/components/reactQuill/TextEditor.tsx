74387ed62d8405a0de597e0c3c1492be82337c46
김준하 <junhakjh@gmail.com>
2023-12-09 22:03:31 +0900

    fix: 게시글 등록시 이미지 등록 방식 변경

--- src/components/reactQuill/TextEditor.tsx
+++ src/components/reactQuill/TextEditor.tsx
@@ -22,10 +22,55 @@ export default function TextEditor({ setContents, receivedContent }: Props) {
       .clipboard.dangerouslyPasteHTML(receivedContent);
   }, [receivedContent]);
 
+  const imageHandler = async () => {
+    const input = document.createElement("input");
+    input.setAttribute("type", "file");
+    input.setAttribute("accept", "image/*");
+    input.click();
+    input.addEventListener("change", async () => {
+      const file = input.files?.[0];
+      if (!file) return;
+      try {
+        const editor = quillRef.current?.getEditor();
+        if (!editor) return;
+
+        const reader = new FileReader();
+        reader.onloadend = async () => {
+          const base64Image = reader.result;
+          if (!base64Image) return;
+          const range = editor.getSelection();
+          if (!range) return;
+          editor.insertEmbed(range.index, "image", base64Image);
+
+          const response = await fetch(base64Image?.toString());
+          const blob = await response.blob();
+          const file = new File([blob], "image.jpg", { type: "image" });
+          const imgId = await uploadImage(file, "article");
+          if (!imgId) throw new Error();
+          const imgURL = `${process.env.NEXT_PUBLIC_SERVER_BASE_URL}attachments/${imgId}/?type=article`;
+
+          const img = document.createElement("img");
+          img.setAttribute("src", imgURL);
+          img.setAttribute("alt", "image");
+          img.setAttribute("width", "0");
+          img.setAttribute("height", "0");
+          const node = document.getElementById("imgLoader") as HTMLElement;
+          node.appendChild(img);
+        };
+        reader.readAsDataURL(file);
+      } catch (error) {
+        console.log(error);
+      }
+    });
+  };
+
   const modules = useMemo(() => {
     return {
       toolbar: {
-        container: "#toolbar"
+        container: "#toolbar",
+        handlers: {
+          image: imageHandler
+        }
       },
       ImageResize: { modules: ["Resize", "DisplaySize"] }
     };
@@ -41,6 +86,7 @@ export default function TextEditor({ setContents, receivedContent }: Props) {
         onChange={setContents}
         placeholder="공유하고 싶은 여행 정보를 기록해주세요!"
       />
+      <div id="imgLoader"></div>
     </div>
   );
 }
