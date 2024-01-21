44f008ae385d1e921b76b08e58107b2caf43c2aa
김준하 <junhakjh@gmail.com>
2024-01-21 22:28:30 +0900

    feat: react query provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,7 @@ import { ProviderContext } from "@/context/ProviderContext";
 import Background from "@/components/layout/Background";
 import Footer from "@/components/layout/Footer";
 import Main from "@/components/layout/Main";
+import ReactQueryProvider from "@/context/ReactQueryProvider";
 import Script from "next/script";
 
 const openSans = Open_Sans({ subsets: ["latin"] });
@@ -24,6 +25,10 @@ export default function RootLayout({
   return (
     <html lang="en" className={openSans.className}>
       <head>
+        <Script
+          defer
+          src={`https://maps.googleapis.com/maps/api/js?key=${process.env.NEXT_PUBLIC_GOOGLE_MAPS_KEY}&libraries=places&callback=initMap`}
+        />
         <link
           href="https://cdn.quilljs.com/1.3.6/quill.snow.css"
           rel="stylesheet"
@@ -31,13 +36,15 @@ export default function RootLayout({
       </head>
       <body className="w-full h-full flex flex-col">
         <Background />
-        <ProviderContext>
-          <AuthContext>
-            <Header />
-            <Main>{children}</Main>
-            <Footer />
-          </AuthContext>
-        </ProviderContext>
+        <ReactQueryProvider>
+          <ProviderContext>
+            <AuthContext>
+              <Header />
+              <Main>{children}</Main>
+              <Footer />
+            </AuthContext>
+          </ProviderContext>
+        </ReactQueryProvider>
         <div id="portal" />
       </body>
     </html>
